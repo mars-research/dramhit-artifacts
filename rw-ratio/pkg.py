09d29948d12551f334453539abf248f4072658d0
David Detweiler <daviddetweiler1@gmail.com>
2023-03-13 15:25:07 -0700

    Correct packaging script

--- rw-ratio/pkg.py
+++ rw-ratio/pkg.py
@@ -1,41 +1,44 @@
+import sys
 import os
-import re
 from collections import defaultdict
+import pathlib
+import re
+
+MOPS_RE = re.compile("set_mops : (\d+(?:.\d+)?), get_mops : ((?:\d+(?:.\d+)?)|inf)")
+
+
+def get_mops(filepath):
+    match = MOPS_RE.search(''.join(open(filepath).readlines()))
+    return float(match[1]), float(match[2])
+
+def main():
+    path = pathlib.Path(sys.argv[1])
+    data = {'cht': {}, 'chtpp': {}, 'queues': defaultdict(lambda : defaultdict(lambda : []))}
+    for file in os.listdir(path):
+        filepath = path.joinpath(file)
+        chunks = file.split('-')
+        kind = chunks[0]
+        if kind not in data.keys():
+            continue
+
+        pread = float(chunks[1])
+        if kind != 'queues':
+            data[kind][pread] = get_mops(filepath)[0]
+        else:
+            n_cons = int(chunks[2])
+            data[kind][pread][n_cons].append(get_mops(filepath)[1])
+
+    for pread in data['queues']:
+        blob = data['queues'][pread]
+        best_mops = 0.0
+        for n_cons in blob:
+            best_mops = max((lambda l : sum(l) / len(l))(blob[n_cons]), best_mops)
+
+        data['queues'][pread] = best_mops
+
+    print('p-read, cht, chtpp, queues')
+    for pread in data['queues']:
+        print(pread, data['cht'][pread], data['chtpp'][pread], data['queues'][pread], sep=', ')
 
 if __name__ == '__main__':
-    data = defaultdict(lambda : {})
-    data['queues'] = defaultdict(lambda : {})
-    mops_re = re.compile("set_mops : (\d+(?:.\d+)?), get_mops : (\d+(?:.\d+)?)")
-    for filename in os.listdir():
-        with open(filename) as file:
-            kind, n = filename.split('-')[1:]
-            n = int(n)
-            for line in file.readlines():
-                result = mops_re.search(line)
-                if result != None:
-                    if 'queues' not in kind:
-                        data[kind][n] = (float(result[1]), float(result[2]))
-                    else:
-                        data['queues'][int(kind[len('queues'):])][n] = (float(result[1]), float(result[2]))
-
-                    break
-                
-            #assert n in data[kind]
-
-    queues = defaultdict(lambda : ([], []))
-    for nc, subdata in data['queues'].items():
-        for pollutes, mops in subdata.items():
-            queues[pollutes][0].append(mops[0])
-            queues[pollutes][1].append(mops[1])
-
-    queues = {n: (max(d[0]), max(d[1])) for n, d in queues.items()}
-    data['queues'] = queues
-
-    finds = {kind: [(n, data[kind][n][1]) for n in data[kind]] for kind in data}
-    finds = {kind: list(zip(*sorted(finds[kind], key=lambda v : v[0]))) for kind in finds}
-    inserts = {kind: [(n, data[kind][n][0]) for n in data[kind]] for kind in data}
-    inserts = {kind: list(zip(*sorted(inserts[kind], key=lambda v : v[0]))) for kind in inserts}
-
-    print('pollutions, queue_set, queue_get, chtpp_set, chtpp_get, cht_set, chtpp_get')
-    for i, p in enumerate(finds['queues'][0]):
-        print(f"{p}, {inserts['queues'][1][i]}, {finds['queues'][1][i]}, {inserts['chtpp'][1][i]}, {finds['chtpp'][1][i]}, {inserts['cht'][1][i]}, {finds['cht'][1][i]}")
+    main()
